f1b5fe98114fd755c3fb88f1551f901630efde35
Benjamin Day <benday@benday.com>
2023-04-01 07:31:14 -0400

    Documentation for command attribute

--- src/Benday.CommandsFramework/CommandAttribute.cs
+++ src/Benday.CommandsFramework/CommandAttribute.cs
@@ -1,9 +1,27 @@
 ﻿namespace Benday.CommandsFramework;
 
+/// <summary>
+/// Add this attribute to a class to indicate that the class contains a command that
+/// is runnable through the commands framework. This attribute provides information 
+/// about the command name, whether it uses async execution, and optionally the human
+/// readable description.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public class CommandAttribute : Attribute
 {
+    /// <summary>
+    /// Name of command. This is the command argument (arg[0]) from the command line.
+    /// This value is used to locate the command to be instantiated and run.
+    /// </summary>
     public string Name { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// True if this command should be run in async mode
+    /// </summary>
     public bool IsAsync { get; set; } = false;
+
+    /// <summary>
+    /// Human readable description of the command.
+    /// </summary>
     public string Description { get; set; } = string.Empty;
 }
