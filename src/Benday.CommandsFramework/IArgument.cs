8e84f97d48c301bcaa5a7edddfe36b0ddb790271
benday <benday@benday.com>
2023-04-04 06:52:44 -0400

    adding more documentation

--- src/Benday.CommandsFramework/IArgument.cs
+++ src/Benday.CommandsFramework/IArgument.cs
@@ -1,15 +1,56 @@
 ﻿namespace Benday.CommandsFramework;
 
+/// <summary>
+/// Interface describing the methods and properties for an argument
+/// </summary>
 public interface IArgument
 {
+    /// <summary>
+    /// Allow or disallow empty values
+    /// </summary>
     bool AllowEmptyValue { get; }
+
+    /// <summary>
+    /// Data type for the argument
+    /// </summary>
     ArgumentDataType DataType { get; }
+
+    /// <summary>
+    /// Human readable description for the argument
+    /// </summary>
     string Description { get; }
+
+    /// <summary>
+    /// Does this argument have a value?
+    /// </summary>
     bool HasValue { get; }
-    bool IsRequired { get;  }
+
+    /// <summary>
+    /// Is this argument required to have a value?
+    /// </summary>
+    bool IsRequired { get; }
+
+    /// <summary>
+    /// Name of the argument on the command line
+    /// </summary>
     string Name { get; set; }
+
+    /// <summary>
+    /// Value for the argument
+    /// </summary>
     string Value { get; }
 
+    /// <summary>
+    /// Validate the argument value against the argument definition information
+    /// </summary>
+    /// <returns>True if the value is valid</returns>
     bool Validate();
+
+    /// <summary>
+    /// Try to set a value to this argument.
+    /// </summary>
+    /// <param name="input">Value to set</param>
+    /// <returns>True if the value could be converted to the appropriate data
+    /// type and was successfully set as the argument value.</returns>
     bool TrySetValue(string input);
 }
--- src/Benday.CommandsFramework/IAsyncCommand.cs
+++ src/Benday.CommandsFramework/IAsyncCommand.cs
@@ -1,6 +1,14 @@
 ﻿namespace Benday.CommandsFramework;
 
+/// <summary>
+/// Interface for defining a command as using async
+/// execution.
+/// </summary>
 public interface IAsyncCommand
 {
+    /// <summary>
+    /// Triggers async execution of this command
+    /// </summary>
+    /// <returns>The async task</returns>
     Task ExecuteAsync();
 }
