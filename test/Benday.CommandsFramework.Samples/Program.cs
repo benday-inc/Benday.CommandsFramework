d465e16a302d0eb68ce60b91185e4355227783a8
Benjamin Day <benday@benday.com>
2024-02-14 09:57:17 -0500

    Refactored sample util to use DefaultProgramOptions

--- test/Benday.CommandsFramework.Samples/Program.cs
+++ test/Benday.CommandsFramework.Samples/Program.cs
@@ -1,71 +1,28 @@
 ﻿using Benday.CommandsFramework;
+using Benday.CommandsFramework.Samples;
+using System.Diagnostics;
+using System.Reflection;
 
-var util = new CommandAttributeUtility();
+namespace Benday.CommandsFramework.Samples;
 
-if (args.Length == 0)
+class Program
 {
-    DisplayUsage(util);
-}
-else
-{
-    var command = util.GetCommand(args, typeof(Program).Assembly);
-
-    if (command == null)
+    static void Main(string[] args)
     {
-        DisplayUsage(util);
-    }
-    else
-    {
-        var attr = util.GetCommandAttributeForCommandName(typeof(Program).Assembly,
-            command.ExecutionInfo.CommandName);
+        var assembly = typeof(SampleCommand1).Assembly;
 
-        if (attr == null)
-        {
-            throw new InvalidOperationException(
-                $"Could not get command attribute for command name '{command.ExecutionInfo.CommandName}'.");
-        }
-        else
-        {
-            if (attr.IsAsync == false)
-            {
-                var runThis = command as ISynchronousCommand;
+        var versionInfo =
+            FileVersionInfo.GetVersionInfo(
+                Assembly.GetExecutingAssembly().Location);
 
-                if (runThis == null)
-                {
-                    throw new InvalidOperationException($"Could not convert type to {typeof(ISynchronousCommand)}.");
-                }
-                else
-                {
-                    runThis.Execute();
-                }
-            }
-            else
-            {
-                var runThis = command as IAsyncCommand;
+        var options = new DefaultProgramOptions();
 
-                if (runThis == null)
-                {
-                    throw new InvalidOperationException($"Could not convert type to {typeof(IAsyncCommand)}.");
-                }
-                else
-                {
-                    var temp = runThis.ExecuteAsync().GetAwaiter();
+        options.Version = $"v{versionInfo.FileVersion}";
+        options.ApplicationName = "Sample Tool using Commands Framework";
+        options.Website = "https://www.benday.com";
 
-                    temp.GetResult();
-                }
-            }
-        }
-    }
-}
+        var program = new DefaultProgram(options, assembly);
 
-void DisplayUsage(CommandAttributeUtility util)
-{
-    var assembly = typeof(Program).Assembly;
-
-    var commands = util.GetAvailableCommandNames(assembly);
-
-    foreach (var command in commands)
-    {
-        Console.WriteLine(command);
+        program.Run(args);
     }
 }
