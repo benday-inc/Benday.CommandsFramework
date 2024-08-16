d19d452bd30c04ead8ef54e97fa564b80c8df67c
Benjamin Day <benday@benday.com>
2024-08-16 11:22:31 -0400

    Adding tests for parsing date time values

---
+++ test/Benday.CommandsFramework.Samples/DateTimeTestCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Benday.CommandsFramework.Samples;
+
+[Command(Name = "datetimetest", Description = "Date time test")]
+public class DateTimeTestCommand : SynchronousCommand
+{
+    public DateTimeTestCommand(CommandExecutionInfo info, ITextOutputProvider outputProvider) :
+        base(info, outputProvider)
+    {
+
+    }
+
+    public override ArgumentCollection GetArguments()
+    {
+        var args = new ArgumentCollection();
+
+        args.AddDateTime("date")
+            .WithDescription("Date")
+            .AsRequired();
+
+        return args;
+    }
+
+    protected override void OnExecute()
+    {
+        var date = Arguments.GetDateTimeValue("date");
+
+        WriteLine($"** SUCCESS **");
+
+        WriteLine($"Date: {date}");
+    }
+}
---
+++ test/Benday.CommandsFramework.Tests/DateTimeTestCommandFixture.cs
@@ -0,0 +1,170 @@
+﻿using Benday.CommandsFramework.Samples;
+
+namespace Benday.CommandsFramework.Tests;
+
+[TestClass]
+public class DateTimeTestCommandFixture
+{
+    [TestInitialize]
+    public void OnTestInitialize()
+    {
+        _SystemUnderTest = null;
+        _OutputProvider = null;
+    }
+
+    private DateTimeTestCommand? _SystemUnderTest;
+
+    private DateTimeTestCommand SystemUnderTest
+    {
+        get
+        {
+            Assert.IsNotNull(_SystemUnderTest);
+
+            return _SystemUnderTest;
+        }
+    }
+
+    private StringBuilderTextOutputProvider? _OutputProvider;
+
+    private StringBuilderTextOutputProvider OutputProvider
+    {
+        get
+        {
+            if (_OutputProvider == null)
+            {
+                _OutputProvider = new StringBuilderTextOutputProvider();
+            }
+
+            return _OutputProvider;
+        }
+    }
+
+
+    [TestMethod]
+    public void CreateAndRun_ValidArgs_MonthDayYear()
+    {
+        // arrange
+        var inputDateString = "12/24/2022";
+        var expectedDateString = "12/24/2022 12:00:00 AM";
+
+        var commandLineArgs = Utilities.GetStringArray(
+            "datetimetest",
+            $"/date:{inputDateString}"
+            );
+
+        var executionInfo = new ArgumentCollectionFactory().Parse(commandLineArgs);
+
+        _SystemUnderTest = new DateTimeTestCommand(executionInfo, OutputProvider);
+
+        // act
+        _SystemUnderTest.Execute();
+
+        // assert        
+        var output = OutputProvider.GetOutput();
+        Console.WriteLine(output);
+        Assert.IsTrue(output.Contains("** SUCCESS **"), "Did not contain expected string");
+        Assert.IsTrue(output.Contains(expectedDateString), $"Did not contain expected string: {expectedDateString}");
+    }
+
+
+    [TestMethod]
+    public void CreateAndRun_ValidArgs_MonthDayYearHourMin_24h()
+    {
+        // arrange
+        var inputDateString = "12/24/2022 14:30";
+        var expectedDateString = "12/24/2022 2:30 PM";
+
+        var commandLineArgs = Utilities.GetStringArray(
+            "datetimetest",
+            $"/date:{inputDateString}"
+            );
+
+        var executionInfo = new ArgumentCollectionFactory().Parse(commandLineArgs);
+
+        _SystemUnderTest = new DateTimeTestCommand(executionInfo, OutputProvider);
+
+        // act
+        _SystemUnderTest.Execute();
+
+        // assert        
+        var output = OutputProvider.GetOutput();
+        Console.WriteLine(output);
+        Assert.IsTrue(output.Contains("** SUCCESS **"), "Did not contain expected string");
+        Assert.IsTrue(output.Contains(expectedDateString), $"Did not contain expected string: {expectedDateString}");
+    }
+
+    [TestMethod]
+    public void CreateAndRun_ValidArgs_MonthDayYearHourMinSec_24h()
+    {
+        // arrange
+        var inputDateString = "12/24/2022 14:30:30";
+        var expectedDateString = "12/24/2022 2:30:30 PM";
+
+        var commandLineArgs = Utilities.GetStringArray(
+            "datetimetest",
+            $"/date:{inputDateString}"
+            );
+
+        var executionInfo = new ArgumentCollectionFactory().Parse(commandLineArgs);
+
+        _SystemUnderTest = new DateTimeTestCommand(executionInfo, OutputProvider);
+
+        // act
+        _SystemUnderTest.Execute();
+
+        // assert        
+        var output = OutputProvider.GetOutput();
+        Console.WriteLine(output);
+        Assert.IsTrue(output.Contains("** SUCCESS **"), "Did not contain expected string");
+        Assert.IsTrue(output.Contains(expectedDateString), $"Did not contain expected string: {expectedDateString}");
+    }
+
+    [TestMethod]
+    public void CreateAndRun_ValidArgs_FileDateTimeUniversal()
+    {
+        // arrange
+
+        var inputDateString = "20240816T1515295960Z";
+        var expectedDateString = "8/16/2024 3:15:29 PM";
+
+        var commandLineArgs = Utilities.GetStringArray(
+            "datetimetest",
+            $"/date:{inputDateString}"
+            );
+
+        var executionInfo = new ArgumentCollectionFactory().Parse(commandLineArgs);
+
+        _SystemUnderTest = new DateTimeTestCommand(executionInfo, OutputProvider);
+
+        // act
+        _SystemUnderTest.Execute();
+
+        // assert        
+        var output = OutputProvider.GetOutput();
+        Console.WriteLine(output);
+        Assert.IsTrue(output.Contains("** SUCCESS **"), "Did not contain expected string");
+        Assert.IsTrue(output.Contains(expectedDateString), $"Did not contain expected string: {expectedDateString}");
+    }
+
+    [TestMethod]
+    public void CreateAndRun_InvalidDate()
+    {
+        // arrange
+        var commandLineArgs = Utilities.GetStringArray(
+            "datetimetest",
+            "/date:notADateValue"
+            );
+
+        var executionInfo = new ArgumentCollectionFactory().Parse(commandLineArgs);
+
+        _SystemUnderTest = new DateTimeTestCommand(executionInfo, OutputProvider);
+
+        // act
+        _SystemUnderTest.Execute();
+
+        // assert        
+        var output = OutputProvider.GetOutput();
+        Console.WriteLine(output);
+        Assert.IsTrue(output.Contains("** INVALID ARGUMENTS **"), "Did not contain expected string");
+    }    
+}
