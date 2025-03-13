2619306b51249a3cfa5dc571d5fd5b095d514f6e
Benjamin Day <benday@benday.com>
2025-03-13 11:45:22 -0700

    added documentation comments

--- src/Benday.CommandsFramework/DataFormatting/TableColumnDefinition.cs
+++ src/Benday.CommandsFramework/DataFormatting/TableColumnDefinition.cs
@@ -1,9 +1,18 @@
 namespace Benday.CommandsFramework.DataFormatting;
 
+/// <summary>
+/// Definition of a column in a table.
+/// </summary>
 public class TableColumnDefinition
 {
+    /// <summary>
+    /// The name of the column.
+    /// </summary>
     public required string Name { get; set; }
 
+    /// <summary>
+    /// The name of the column, padded to the width of the longest value in the column.
+    /// </summary>
     public string NamePadded
     {
         get
@@ -12,8 +21,14 @@ public class TableColumnDefinition
         }
     }
 
+    /// <summary>
+    /// The width of the longest value in the column.
+    /// </summary>
     public int WidthOfLongestValue { get; set; }
 
+    /// <summary>
+    /// The width of the column.  This is the greater of the length of the column name or the width of the longest value.
+    /// </summary>
     public int Width
     {
         get
@@ -22,6 +37,9 @@ public class TableColumnDefinition
         }
     }
 
+    /// <summary>
+    /// Is the column name longer than the longest value in the column?
+    /// </summary>
     public bool IsColumnNameLongerThanLongestValue
     {
         get
@@ -31,7 +49,8 @@ public class TableColumnDefinition
     }
 
     /// <summary>
-    /// Check the length of the value to see if it is longer than the current longest value
+    /// Check the length of the value to see if it is longer than the current longest value. 
+    /// If it is, update the WidthOfLongestValue property.
     /// </summary>
     /// <param name="newValue"></param>
     public void CheckValueLength(string newValue)
--- src/Benday.CommandsFramework/DataFormatting/TableFormatter.cs
+++ src/Benday.CommandsFramework/DataFormatting/TableFormatter.cs
@@ -3,6 +3,9 @@ using System.Text;
 
 namespace Benday.CommandsFramework.DataFormatting;
 
+/// <summary>
+/// This class formats data as a table for display in the console.  It also provides some basic filtering capabilities.
+/// </summary>
 public class TableFormatter
 {
     public TableFormatter()
@@ -10,9 +13,21 @@ public class TableFormatter
         Columns = new List<TableColumnDefinition>();
     }
 
+    /// <summary>
+    /// The columns in the table.
+    /// </summary>
     public List<TableColumnDefinition> Columns { get; }
+
+    /// <summary>
+    /// The data in the table. Each string[] represents a row in the table.
+    /// </summary>
     public List<string[]> Data { get; } = new List<string[]>();
 
+    /// <summary>
+    /// Add a column to the table.
+    /// </summary>
+    /// <param name="columnName">Name of the column</param>
+    /// <returns></returns>
     public TableColumnDefinition AddColumn(string columnName)
     {
         var newItem = new TableColumnDefinition()
@@ -25,6 +40,13 @@ public class TableFormatter
         return newItem;
     }
 
+    /// <summary>
+    /// Add data to the table.
+    /// </summary>
+    /// <param name="data">The column values for this row.</param>
+    /// <exception cref="ArgumentNullException">This is thrown if the data passed in is null</exception>
+    /// <exception cref="ArgumentException">This is thrown if the number of values does not match the 
+    /// number of configured columns for the table.</exception>
     public void AddData(params string[] data)
     {
         if (data == null)
@@ -59,6 +81,10 @@ public class TableFormatter
         }
     }
 
+    /// <summary>
+    /// Get the table formatted as a string.
+    /// </summary>
+    /// <returns></returns>
     public string FormatTable()
     {
         var builder = new StringBuilder();
@@ -102,7 +128,7 @@ public class TableFormatter
     }
 
     /// <summary>
-    /// Add data to the table if any of the values contain the filter.
+    /// Add data to the table if any of the column values contain the filter.
     /// </summary>
     /// <param name="filter"></param>
     /// <param name="data"></param>    
