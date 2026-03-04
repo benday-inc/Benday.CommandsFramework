394c2f77b40d6e464301fbe8a73e1e72bd7deda8
benday <benday@benday.com>
2026-03-04 13:05:53 -0500

    fixing weird case problem in project name by deleting the folder

--- src/Benday.CommandsFramework.CmdUI/Models/ToolInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Benday.CommandsFramework.CmdUi.Models;
-
-public class ToolInfo
-{
-    public string ToolName { get; set; } = string.Empty;
-    public string Version { get; set; } = string.Empty;
-    public bool SupportsJson { get; set; }
-    public List<ToolCommandInfo> Commands { get; set; } = new();
-}
